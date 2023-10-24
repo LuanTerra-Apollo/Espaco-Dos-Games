bf1b2473dfb09aa3af93fd0e9fba91590a24cfed
LuanTerra-Apollo <luan_terra12@hotmail.com>
2023-10-24 16:36:39 -0300

    Subindo o Esqueleto do Projeto

---
+++ espacoDosGames/src/main/java/com/espacodosgames/espacodosgames/EspacodosgamesApplication.java
@@ -0,0 +1,13 @@
+package com.espacodosgames.espacodosgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspacodosgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspacodosgamesApplication.class, args);
+	}
+
+}
